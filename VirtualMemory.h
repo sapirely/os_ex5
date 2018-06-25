c79b2b9f743ecae4cc69296ba03064eec1b65c83
Naama Kessler <naamak10@gmail.com>
2018-06-25 15:34:27 +0300

    sketch

--- VirtualMemory.h
+++ VirtualMemory.h
@@ -1,7 +1,19 @@
 #pragma once
 
+// ------------------------------------- includes ------------------------------------- //
+//#include <string>
 #include "MemoryConstants.h"
+#include <cstdio>
 
+// ------------------------------------- PT in the tree ------------------------------------- //
+struct Table
+{
+    int* table;
+    int maxReferencedFrame;
+}Table;
+
+
+// ------------------------------------- library methods ------------------------------------- //
 /*
  * Initialize the virtual memory
  */
@@ -25,4 +37,33 @@ int VMread(uint64_t virtualAddress, word_t* value);
 
 int VMwrite(uint64_t virtualAddress, word_t value);
 
+
+// ------------------------------------- inner methods ------------------------------------- //
+
+/**
+ * Translates a decimal address to binary.
+ * @param address
+ * @return
+ */
+int* addressToBin(int address);
+
+/**
+ * Interpret a binary address as an index + offset.
+ * @param address
+ * @return [index, offset] array of ints
+ */
+int* addressInterpreter(int address);
+
+/**
+ * Find page with max cyclic distance from the target page.
+ * @param targetPage
+ * @return index of page with max cyclic distance from the target page.
+ */
+int maxCyclicDist(int targetPage);
+
+/**
+ * @return idx of an unused frame in the physical memory (if an empty table was found, the method
+ * initializes its entries to 0 and returns an idx of a frame in the table).
+ */
+int findUnusedFrame();
 
